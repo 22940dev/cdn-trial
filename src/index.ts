9dc6f659dcd3c57af5760cbe1f31b182aa63c149
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-13 09:23:12 -0800

    feat: establish initial cdn
    Sets up the initial routing for the CDN.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,54 @@
-console.log("It's aliiiiiiiive");
+import express from "express";
+import { readFile } from "fs/promises";
+import http from "http";
+import https from "https";
+import path from "path";
+(async () => {
+  const CDN = express();
+
+  console.log(__dirname);
+
+  CDN.use("/content", express.static(path.join(__dirname + "/assets")));
+
+  CDN.get("/", (req, res) => {
+    res.send("CDN works!");
+  });
+
+  CDN.use("/*", (req, res) => {
+    res.send("File not found!");
+  });
+
+  const httpServer = http.createServer(CDN);
+
+  const httpPort = process.env.NODE_ENV === "production" ? 80 : 8080;
+
+  httpServer.listen(httpPort, () => {
+    console.log(`http server listening on port ${httpPort}`);
+  });
+
+  if (process.env.NODE_ENV === "production") {
+    const privateKey = await readFile(
+      "/etc/letsencrypt/live/cdn.nhcarrigan.com/privkey.pem",
+      "utf8"
+    );
+    const certificate = await readFile(
+      "/etc/letsencrypt/live/cdn.nhcarrigan.com/cert.pem",
+      "utf8"
+    );
+    const ca = await readFile(
+      "/etc/letsencrypt/live/cdn.nhcarrigan.com/chain.pem",
+      "utf8"
+    );
+
+    const credentials = {
+      key: privateKey,
+      cert: certificate,
+      ca: ca,
+    };
+
+    const httpsServer = https.createServer(credentials, CDN);
+    httpsServer.listen(443, () => {
+      console.log("https server listening on port 443");
+    });
+  }
+})();
